3a87c435d307c99e5b01ffe9d997988cc81cb54a
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-07-07 18:07:14 +0300

    Add main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <Header />;
+  const isLogin = true;
+  const user = {
+    specOffers: "You have -20% for all laptops",
+  };
+
+  return (
+    <>
+      <Header isLogin={isLogin} />
+      <Main isLogin={isLogin} user={user} />
+    </>
+  );
 }
 
 export default App;
