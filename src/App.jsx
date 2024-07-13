2cd48375d3fbd07cddf281e250b72c2883d0e44a
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-07-13 17:16:59 +0300

    add classnames

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,7 @@
-import { useState } from "react";
-import Counter from "./components/Counter";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import NavLinks from "./components/NavLinks";
 
 function App() {
-  const [isLight, setIsLight] = useState(false);
-
-  const isLogin = false;
-  const user = {
-    specOffers: "You have -20% for all laptops",
-  };
-
-  const buttonStyle = {
-    color: isLight ? "yellow" : "grey",
-  };
-
-  const pageContainerStyle = {
-    minHeight: "100vh",
-    color: isLight ? "black" : "white",
-    backgroundColor: isLight ? "white" : "black",
-  };
-
-  function changeTheme() {
-    setIsLight((oldIsLight) => !oldIsLight);
-  }
-
-  return (
-    <div style={pageContainerStyle}>
-      <Header isLogin={isLogin} />
-      <button onClick={changeTheme} style={buttonStyle}>
-        <i className="fa-solid fa-sun"></i>
-      </button>
-      <Main isLogin={isLogin} user={user} />
-      <Counter />
-    </div>
-  );
+  return <NavLinks />;
 }
 
 export default App;
