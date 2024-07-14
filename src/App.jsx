d58d99d4ebd950882375bc8d542dc10d2a77a897
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-07-14 15:15:22 +0300

    Create nav links usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
-import NavLinks from "./components/NavLinks";
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
+import CategItem from "./components/CategItem";
+
+// const NAV_LINK = {
+//   icon: "/icons/learn.svg",
+//   menuItem: "Навчання",
+//   link: "#",
+//   hasNotifications: false,
+//   isActive: false,
+// };
 
 function App() {
-  return <NavLinks />;
+  // const [navLink, setNavLink] = useState(NAV_LINK);
+  return (
+    <>
+      <LoginForm />
+      {/* <NavLinks /> */}
+      {/* <NavLink navLink={navLink} setNavLink={setNavLink} /> */}
+      {/* <CategItem /> */}
+    </>
+  );
 }
 
 export default App;
+
+// function NavLink({ navLink, setNavLink }) {
+//   const linkStyle = {
+//     border: navLink.isActive ? "5px solid blue" : "",
+//   };
+
+//   function changeActive() {
+//     const navLinkCopy = { ...navLink };
+//     navLinkCopy.isActive = !navLinkCopy.isActive;
+//     setNavLink(navLinkCopy);
+//   }
+
+//   return (
+//     <div key={navLink.icon}>
+//       <a onClick={changeActive} style={linkStyle} href={navLink.link}>
+//         <img src={navLink.icon} /> {navLink.menuItem}
+//       </a>
+//     </div>
+//   );
+// }
